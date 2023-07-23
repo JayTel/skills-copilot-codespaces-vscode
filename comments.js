312b0a56a4ddc778fc0355723469ad2a4d4621b9
JayTel <140227106+JayTel@users.noreply.github.com>
2023-07-23 00:08:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,76 @@
-// Create a Web server that can listen to requests for /comments.json and serve up 
-// our JavaScript object. Remember to set the Content-Type header to application/json.
-var http = require('http');
-var fs = require('fs');
-var port = 8080;
+// Create Web server 
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    fs.readFile('./comments.json', 'utf8', function (err, data) {
-        if (err) throw err;
-        res.end(data);
+// Import express module
+const express = require('express');
+// Import path module
+const path = require('path');
+// Import body-parser module
+const bodyParser = require('body-parser');
+
+// Import mongoose module
+const mongoose = require('mongoose');
+
+// Create express object
+const app = express();
+
+// Import Comment model
+const Comment = require('./models/comment');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/commentDB', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.log(err);
     });
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+// Set views folder to 'views'
+app.set('views', 'views');
+
+// Use body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Use static middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set port
+const port = process.env.PORT || 3000;
+
+// Get comments from database
+app.get('/', (req, res, next) => {
+    Comment.find()
+        .then(comments => {
+            res.render('index', {
+                comments: comments
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
-server.listen(port, function () {
-    console.log('Server listening on http://localhost:%s', port);
+// Post comment
+app.post('/comment', (req, res, next) => {
+    // Create new comment
+    const comment = new Comment({
+        name: req.body.name,
+        comment: req.body.comment
+    });
+    // Save comment to database
+    comment.save()
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
